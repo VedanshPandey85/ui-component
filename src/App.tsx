f683ee7ec66e40e727cd71987f525cb3988c9d63
Vedansh Pandey <202312085@daiict.ac.in>
2026-02-04 23:59:14 +0530

    Add comment to App.js main entry point
    Add a comment indicating the main entry point of the app.

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { ThemeProvider, useThemeContext } from './components/ThemeProvider/ThemeProvider';
 import { Layout } from './components/Layout/Layout';
-
+//this the app .js main entry point
 function App() {
   return (
     <ThemeProvider>
